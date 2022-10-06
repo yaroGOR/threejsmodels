05d135236298e24fc22eb698cb0ee3df68276489
Yarik <36842297+yaroGOR@users.noreply.github.com>
2022-10-06 17:32:18 +0300

    upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Model from "./Model";
+
+export default function App() {
+  
+  return (
+    <div className="App">
+      <Canvas >
+        <Suspense fallback={null}>
+          <Model />
+        
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    <p>Lera is the most beautyful girl in the world</p>
+    </div>
+  );
+}
